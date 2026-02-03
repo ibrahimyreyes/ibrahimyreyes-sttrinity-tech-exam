26e5af61a009e54a3c38e56660463e034cea068c
ibrahimyreyes <ivrahim.reyes@gmail.com>
2026-02-03 22:12:43 +0800

    Cleanup E2E test: remove extra whitespace in validate-place-order-with-bypass-login.spec.ts

--- src/tests/testsuites/E2E/validate-place-order-with-bypass-login.spec.ts
+++ src/tests/testsuites/E2E/validate-place-order-with-bypass-login.spec.ts
@@ -13,11 +13,13 @@ test.describe('E2E scenario for placing an order with bypass login', { tag: '@E2
     const randomFirstName = await randomDataGenerator.generateRandomFirstName();
     const randomLastName = await randomDataGenerator.generateTimestamp();
 
+
     await test.step('Login', async () => {
-      await actionUtils.navigateTo(APPCONFIG.Prd.SauceDemo.App.URL + '/inventory.html');
+      await actionUtils.navigateTo(APPCONFIG.Prd.SauceDemo.App.URL+'/inventory.html');
       await productPage.click_add_to_cart('Sauce Labs Backpack');
       await headerPage.click_addToCartIcon();
       await cartPage.click_checkout();
+      
     });
-  });
+  }); 
 });
